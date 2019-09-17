39911abd61c9ec47a0b0f3cf7142ce56ba45efd6
aaronaanderson <aaron.arthur.anderson@gmail.com>
2019-09-17 00:13:59 -0700

    not updating, hmm

--- src/generators/oscillators/TSine.hpp
+++ src/generators/oscillators/TSine.hpp
@@ -1,22 +1,22 @@
-//#ifndef TrivialOscillators_hpp
-//#define TrivialOscillators_hpp
+#ifndef TSine_hpp
+#define TSine_hpp
 
 #include <stdio.h>
-#define _USE_MATH_DEFINES
-#include <math.h>
-
-//#endif /* TrivialOscillators_hpp */
+#define _USE_MATH_DEFINES  //so we can use M_PI
+#include <math.h> //so we can use sin()
+#include "../../pdlSettings.hpp"//so we can access sampleRate and bufferSize
 
 class pdlTSine {//Pedal Trivial Sine Oscillator
     public:
     pdlTSine();//constructor, defined in the cpp
-
+    pdlTSine(float frequency);//option to set frequency on construction
+    
     inline float generate(){//return a float even if you don't use it
         //inline functions must be located in the header file, so 
         //we will define it here. 
 
-        currentSample = sin(phase) * amplitude;
-        phase += phaseIncrement;
+        currentSample = sin(phase) * amplitude;//calculate single sample
+        phase += phaseIncrement;//increment phase for the next sample
     }
 
     inline float* generateBlock(){//it is best to do all 
@@ -24,13 +24,14 @@ class pdlTSine {//Pedal Trivial Sine Oscillator
         //jumping around looking for data
 
         if(currentBlock != nullptr){//if we don't have a local currentBlock yet, 
-            currentBlock = new float[pdlSettings::bufferSize];
+            currentBlock = new float[pdlSettings::bufferSize];//create a new array of floats
         }
-        for(int i = 0; i < pdlSettings::bufferSize; ++i){
-            currentBlock[i] = sin(phase) * amplitude;
-            phase += phaseIncrement;
+
+        for(int i = 0; i < pdlSettings::bufferSize; ++i){//for every sample in the buffer
+            currentBlock[i] = sin(phase) * amplitude;//calculate the next sample
+            phase += phaseIncrement;//increment phase
         }
-        return currentBlock;
+        return currentBlock;//returns pointer to the begining of this block
     }
 
     //"setters"
@@ -52,3 +53,4 @@ class pdlTSine {//Pedal Trivial Sine Oscillator
     double phaseIncrement;//extra precision necessary 
 }
 
+#endif 
