05403a2a4e61067c69acb5a3f78a283098e60695
aaronaanderson <aaron.arthur.anderson@gmail.com>
2019-10-02 11:42:13 -0700

    added linear interp

--- include/pedal/Interpolation.hpp
+++ include/pedal/Interpolation.hpp
@@ -1,10 +1,13 @@
 #ifndef Interpolation_hpp
 #define Interpolation_hpp
 
+
+//the least expensive, yet still effective option for interpolation
 inline float linearInterpolation(float inputSample, float previousSample, float nextSample){
-  float currentSample = 0;
-   
-  return currentSample;
+  //determine position between previous and next sample
+  float position = inputSample  - int(inputSample);
+  //the result is a weighted sum based on this position
+  return (previousSample * (1.0f-position)) + (nextSample * position);
 }
 
 
