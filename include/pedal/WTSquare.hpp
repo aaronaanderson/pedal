5591fe82f35de95a8dee5b6de8a2406a616e9b83
aaronaanderson <aaron.arthur.anderson@gmail.com>
2019-10-07 13:35:56 -0700

    added WTSquare

---
+++ include/pedal/WTSquare.hpp
@@ -0,0 +1,67 @@
+#ifndef WTSquare_hpp
+#define WTSquare_hpp
+
+#include <iostream>
+#include "pdlSettings.hpp"
+#include <cmath>
+#include "Interpolation.hpp"
+
+#define NUM_TABLES 10
+#define TABLESIZE 2048
+
+class SquareTable{
+  private://class members are private by default; added for clarity
+  static SquareTable* instance;//store a pointer to an instance of the table
+  //The next value is simply 'what frequency would be played if 1 sample of 
+  //of the table was played per 1 sample of the audio out.
+  float fundamentalFrequency = (float)pdlSettings::sampleRate/float(TABLESIZE);
+  SquareTable();//constructor is private, which is unusual 
+  ~SquareTable();
+  void normalizeTables();//bring tables a range to -1 to 1
+  float** table;//storage of the table (two dimmensional array of floats);
+  float currentLowestFrequency;//used to keep track of table frequencies
+  float* lowFrequencyList;//an array of the base frequencies per table
+  float nyquist;//nyquist frequency is 1/2 sr
+  
+  public:
+  static SquareTable* getInstance();//provide access to the single instance of the table
+  float** getTable();//return a pointer to (a pointer to) the table
+  float* getLowFrequencyList();
+  float getFundamentalFrequency();//return table fundamental
+  int getTableSize();//return table size (-1)
+};
+
+class WTSquare{
+  public://everything listed after this is public
+  WTSquare();//constructor, defined in the cpp
+  WTSquare(float frequency);//option to set frequency on construction
+  ~WTSquare();//deconstructor (may be needed to free memory)
+  float generateSample();//generate and return a single sample
+  float* generateBlock();//generate and return a block of samples
+
+  //"setters"
+  void setFrequency(float newFrequency);
+  void setPhase(float newPhase);
+  void setAmplitude(float newAmplitude);
+
+  //"getters"
+  float getFrequency();
+  float getPhase();
+  float getAmplitude();
+  float getSample();
+  float* getBlock();
+    
+  private://everything after this is private (cannot be accessed externally without
+  //a "getter" or a "setter"
+  //best practice to leave inner workings private
+
+  SquareTable* instance = SquareTable::getInstance();
+  float currentTable;
+  float whichTable(float frequency);//input frequency, output which table to read
+  float frequency, phase, amplitude;//standard oscillator variables
+  float currentSample;//current working sample
+  float* currentBlock;//current working block of samples
+  double phaseIncrement;//extra precision necessary 
+};
+
+#endif
---
+++ src/generators/oscillators/WTSquare.cpp
@@ -0,0 +1,183 @@
+#include "pedal/WTSquare.hpp"
+
+//Sine Table=====================================================
+SquareTable::SquareTable(){//when it is time to build a table (constructor)
+  nyquist = (float)pdlSettings::sampleRate * 0.5f;
+  fundamentalFrequency = (float)pdlSettings::sampleRate/float(TABLESIZE - 1);
+  lowFrequencyList = new float[NUM_TABLES];//allocate space for this list
+  currentLowestFrequency = 20.0f;//20 will be considered the lowest frequency;
+  for(int i = 0; i < NUM_TABLES; i++){//for each table 
+    lowFrequencyList[i] = currentLowestFrequency;//keep track of the lowest frequency per octave
+    currentLowestFrequency *= 2.0f;//advance to the next octave
+    //20,40,80,160,320,640,1280,2560,5120,10240
+  }
+
+  //allocate space for the 2D array of tables
+  table = new float*[NUM_TABLES];//allocate each table slot
+  for(int i = 0; i < NUM_TABLES; i++){//for each table slot,
+    table[i] = new float[TABLESIZE];//allocate space for the table
+    for(int j = 0; j < TABLESIZE; j++){//for every sample in the table
+      table[i][j] = 0.0f;//0 the memory   
+    }
+  }
+    
+  //generate the harmonics
+  for(int i = 0; i < NUM_TABLES; i++){//for each table
+    //how many haromincs are available before aliasing?
+    //add harmonics while under the nyquist frequency
+    int availableHarmonics = 1;//start with a single available harmonic
+    while(lowFrequencyList[i]*(availableHarmonics + 1) < nyquist){//test va
+      if(availableHarmonics > getTableSize() * 0.5){//check the table nyquist
+        break;//can't store harmonics greater than 1/2 the fundamental 
+      }
+      availableHarmonics += 1;//add the next harmonic
+    }
+    
+    for(int j = 0; j < TABLESIZE; j++){//for each sample in that table
+      table[i][j] = 0.0f;
+      for(int harmonic = 1;harmonic < availableHarmonics; harmonic+=2){//for each available harmonic
+        float harmonicPhase = (j * 6.2831853f * harmonic)/float(getTableSize());
+        float harmonicAmplitude = 1.0f/float(harmonic);
+        table[i][j] += std::sin(harmonicPhase) * harmonicAmplitude;
+      }
+    }
+    //normalize the table
+    normalizeTables(); 
+  }
+
+  //normalize the table
+  normalizeTables(); 
+}
+
+SquareTable::~SquareTable(){//deconstructor (what to do when done)
+  delete[] lowFrequencyList;
+  for(int i = 0; i < NUM_TABLES; i++){
+    delete[] table[i];
+  }
+  delete[] table;
+}
+
+void SquareTable::normalizeTables(){
+  for(int i = 0; i < NUM_TABLES; i++){//for each table
+    float largestValue = 0.0f;//TODO this should really be -inf
+    //find the largest value in the buffer
+    for(int j = 0; j < TABLESIZE; j++){
+      largestValue = fmax(fabs(table[i][j]), largestValue);
+    }
+    //Do some math, largestValue*scalarValue=1.0
+    float scalarValue = 1.0f/largestValue;
+    //scale the entire buffer uniformly by this scalar value
+    for(int j = 0; j < TABLESIZE; j++){
+      table[i][j] *= scalarValue;
+    } 
+  }
+}
+
+float* SquareTable::getLowFrequencyList(){return lowFrequencyList;}
+//since the construct was private, an access method must be included
+SquareTable* SquareTable::getInstance(){
+  if(instance == nullptr){
+    instance = new SquareTable();
+  }
+  return instance;
+}
+//access to the data in the table
+float** SquareTable::getTable(){return table;}
+float SquareTable::getFundamentalFrequency(){return fundamentalFrequency;}
+int SquareTable::getTableSize(){
+  //tell the player that the wave table is 1 sample smaller, for linear interpolation
+  return TABLESIZE - 1;
+}
+//initiate the static pointer as a nullptr
+SquareTable* SquareTable::instance = nullptr;
+
+//WaveTableSine==================================================
+//Constructors and Deconstructors=========
+WTSquare::WTSquare(){
+  setFrequency(440.0f);
+  setPhase(0.0f);
+  setAmplitude(1.0f);
+}
+WTSquare::WTSquare(float initialFrequency){
+  setFrequency(initialFrequency);
+  setPhase(0.0f);
+  setAmplitude(1.0f);
+}
+WTSquare::~WTSquare(){
+  delete[] currentBlock;
+}
+//Basic Functionallity of class==========
+float WTSquare::generateSample(){
+  float** table = instance->getTable();
+  // store which tables will be used (y axis of 2D array)
+  const int lowTableIndex = (int)currentTable;
+  const int highTableIndex = (lowTableIndex < NUM_TABLES-1)? lowTableIndex+1 : lowTableIndex; 
+  // access the necessary tables from the list of tables
+  const float* lowTable = table[lowTableIndex];
+  const float* highTable = table[highTableIndex];
+  // find the phase (x axis of 2D array)
+  const int previousIndex = (int)phase;
+  const int nextIndex = (previousIndex + 1) % TABLESIZE; // wrap phase idx
+  //this is a process known as bilinear interpolation (2D linear interpolation)
+  const float s0 = linearInterpolation(phase, lowTable[previousIndex], lowTable[nextIndex]);
+  const float s1 = linearInterpolation(phase, highTable[previousIndex], highTable[nextIndex]);
+  currentSample = linearInterpolation(currentTable, s0, s1);;
+  currentSample *= amplitude;//scale for amplitude
+  phase += (float)phaseIncrement;//progress phase
+  int tableSize = instance->getTableSize()-1;//account for the extra sample
+  if(phase >= tableSize){phase -= tableSize;}                 
+  if(phase <= 0.0f){phase += tableSize;}
+  return currentSample;//return results
+}
+
+float* WTSquare::generateBlock(){
+  if(currentBlock == nullptr){//if the block hasn't been allocated
+    currentBlock = new float[pdlSettings::bufferSize];//allocate the block
+  }
+  for(int i= 0; i < pdlSettings::bufferSize; i++){//for every sample in block
+    currentBlock[i] = generateSample();//assign the next sample
+  }
+  return currentBlock;
+}
+float WTSquare::whichTable(float testFrequency){//essentially the Y value of a 2D array
+  float* frequencyList = instance->getLowFrequencyList();//get the list of table frequencies
+  //boundry check
+  if(testFrequency > frequencyList[NUM_TABLES-1]){//if the frequency is > than the highest table
+    currentTable = NUM_TABLES-1;
+    return float(currentTable);
+  }else if(testFrequency < frequencyList[0]){//if the frequency is < than the lowest table
+    currentTable = 0.0f;
+    return 0.0f;
+  }
+  int i = 0;//create an index
+  while(testFrequency > frequencyList[i]){//eventually find a fit
+    if(testFrequency < frequencyList[i+1]){//if the next lowestFrequency is too high
+      float positionBetweenTables = ((testFrequency/frequencyList[i]) - 1.0f);//(0.0 - 1.0)
+      return i + positionBetweenTables;
+    }else{//if the next increment is not too hight
+      i++;//increase the increment
+    }
+  }
+  return 0;//windows compilers force this to be here
+}
+//Getters and Setters==================
+void WTSquare::setFrequency(float newFrequency){
+  frequency = newFrequency;
+  currentTable = whichTable(frequency);
+  phaseIncrement = frequency/float(instance->getFundamentalFrequency());
+}
+void WTSquare::setPhase(float newPhase){//expecting 0-TWO_PI
+  phase = std::fmod(std::fabs(newPhase), 6.2831853072f);//wrap to 0 -TWO_PI
+  float scalar = instance->getTableSize()/6.2831853072f;
+  phase = phase * scalar;//map 0-TWO_PI to 0 - tablSize
+}
+void WTSquare::setAmplitude(float newAmplitude){amplitude = newAmplitude;}
+float WTSquare::getFrequency(){return frequency;}
+float WTSquare::getPhase(){
+  return (phase * 6.2831853072f)/float(instance->getTableSize());
+}
+float WTSquare::getAmplitude(){return amplitude;}
+float WTSquare::getSample(){return currentSample;}
+float* WTSquare::getBlock(){return currentBlock;}
+
+
