e05339df7d79d6aab0b93fd07d40b2c7075fcf18
aaronaanderson <aaron.arthur.anderson@gmail.com>
2019-10-07 19:18:26 -0700

    finished main framework

--- include/pedal/ImpulseGenerator.hpp
+++ include/pedal/ImpulseGenerator.hpp
@@ -1,4 +1,32 @@
 #ifndef ImpulseGenerator_hpp
 #define ImpulseGenerator_hpp
 
+#include "pdlSettings.hpp"
+
+class ImpulseGenerator{
+  public:
+  ImpulseGenerator();
+  ImpulseGenerator(float initialFrequency);
+  ~ImpulseGenerator();
+
+  float generateSample();
+  float* generateBlock();
+  
+  void setFrequency(float newFrequency);
+  void setMaskChance(float newMaskChance);
+  void setDeviation(float newDeviation);
+  
+  float getSample();
+  float* getBlock();
+  float getFrequency();
+  float getMaskChance();
+  float getDeviation();
+  
+  private:
+  float frequency;
+  float maskChance;
+  float deviation;//deviation from periodicity
+  float currentSample;
+  float* currentBlock;
+};
 #endif
--- src/generators/ImpulseGenerator.cpp
+++ src/generators/ImpulseGenerator.cpp
@@ -1,1 +1,34 @@
 #include "pedal/ImpulseGenerator.hpp"
+
+//Constructors and Deconstructors=========
+ImpulseGenerator::ImpulseGenerator(){
+
+}
+ImpulseGenerator::ImpulseGenerator(float initialFrequency){
+
+}
+ImpulseGenerator::~ImpulseGenerator(){
+    delete[] currentBlock;
+}
+//core functionality======================
+float ImpulseGenerator::generateSample(){
+
+}
+float* ImpulseGenerator::generateBlock(){
+
+}
+//Getters and Setters
+void ImpulseGenerator::setFrequency(float newFrequency){
+
+}
+void ImpulseGenerator::setMaskChance(float newMaskChance){
+
+}
+void ImpulseGenerator::setDeviation(float newDeviation){
+
+}
+float ImpulseGenerator::getSample(){return currentSample;}
+float* ImpulseGenerator::getBlock(){return currentBlock;}
+float ImpulseGenerator::getFrequency(){return frequency;}
+float ImpulseGenerator::getMaskChance(){return maskChance;}
+float ImpulseGenerator::getDeviation(){return deviation;}
