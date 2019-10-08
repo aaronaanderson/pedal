004cebf353728bbaa7e726f27573fa326fdd559b
aaronaanderson <aaron.arthur.anderson@gmail.com>
2019-10-07 17:44:31 -0700

    added main functionality

--- include/pedal/HanningWindow.hpp
+++ include/pedal/HanningWindow.hpp
@@ -2,26 +2,33 @@
 #define HanningWindow_hpp
 
 #include "pdlSettings.hpp"
+#include "math.h"
 
 class HanningWindow{
   public:
   HanningWindow();
+  HanningWindow(float initialDuration);
   ~HanningWindow();
 
   float generateSample();
   float* generateBlock();
-  static float sampleFromPhase(float newPhase);//expecting 0-1
-  
+  static inline float sampleFromPhase(float inputPhase){
+    return 0.5f * (1.0f - std::cos((2.0f * 3.1415926 * inputPhase)/1.0f));
+  }
+  void setTrigger(bool newTrigger);
   void setDuration(float newDuration);
+  void setPhase(float newPhase);
 
   float getCurrentSample();
   float* getCurrentBlock();
   float getDuration();
+  double getPhase();
 
   private:
-  
+  bool trigger;
+  bool active;
   float duration;
-  double phaseIncrement;
+  double phase, phaseIncrement;
   float currentSample;
   float* currentBlock;
 };
--- src/generators/Windows/HanningWindow.cpp
+++ src/generators/Windows/HanningWindow.cpp
@@ -1,16 +1,28 @@
-#include "pedal/Windows.hpp"
+#include "pedal/HanningWindow.hpp"
 
+//Constructors and Deconstructors=============
 HanningWindow::HanningWindow(){
-
+  setDuration(1000.0f);
+  trigger = false;
+}
+HanningWindow::HanningWindow(float initialDuration){
+  setDuration(initialDuration);
+  trigger = false;
 }
 HanningWindow::~HanningWindow(){
   delete[] currentBlock;
 }
-
+//Core functionality===========================
 float HanningWindow::generateSample(){
-  
+  if(active){
+    currentSample = sampleFromPhase(phase);
+    phase += phaseIncrement;
+    if(phase > 1.0f){
+      phase = 0.0f;
+      active = false;
+    }
+  } 
 }
-
 float* HanningWindow::generateBlock(){
   if(currentBlock == nullptr){
     currentBlock = new float[pdlSettings::bufferSize];
@@ -20,3 +32,21 @@ float* HanningWindow::generateBlock(){
   }
   return currentBlock;
 }
+
+//Getters and Setters===============================
+void HanningWindow::setTrigger(bool newTrigger){
+  if(trigger == false && newTrigger == true){//if it was off but now will be on
+    phase = 0.0;//reset
+  }
+  trigger = newTrigger;//update the trigger status
+}
+void HanningWindow::setDuration(float newDuration){
+  duration = newDuration;
+  phaseIncrement = 1.0f/(pdlSettings::sampleRate*(duration*0.001));
+}
+void HanningWindow::setPhase(float newPhase){phase = newPhase;}
+
+float HanningWindow::getCurrentSample(){return currentSample;}
+float* HannngWindow::getCurrentBlock(){return currentBlock;}
+float HanningWindow::getDuration(){return duration;}
+double HanningWindow::getPhase(){return phase;}
