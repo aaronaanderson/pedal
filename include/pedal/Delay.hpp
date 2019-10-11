58ff1c2a661a267e3d3d8fd307082886612527d6
aaronaanderson <aaron.arthur.anderson@gmail.com>
2019-10-11 10:13:01 -0700

    added Delay class

---
+++ include/pedal/Delay.hpp
@@ -0,0 +1,15 @@
+#ifndef Delay_hpp
+#define Delay_hpp
+
+#include "CircularBuffer.hpp"
+#include "BufferTap.hpp"
+#include "pdlSettings.hpp"
+
+class Delay{
+  public:
+  Delay();
+  ~Delay();
+
+  private:
+};
+#endif
