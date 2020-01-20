24cc0ed7b5e9940205eb1b7b68804d7fd697f2b6
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-01-19 18:00:21 -0800

    added debug

---
+++ include/pedal/DebugTool.hpp
@@ -0,0 +1,28 @@
+/*
+A debugging tool specifically useful for audio 
+thread debugging
+*/
+#ifndef DebugTool_hpp
+#define DebugTool_hpp
+
+#include <iostream>
+#include "pdlSettings.hpp"
+
+class DebugTool{
+  public:
+  //DebugTool();
+  //~DebugTool();
+  //void update(int currentFrame);//update the frame flags
+  template <class T>
+  static void printOncePerBuffer(T whatToPrint, int currentFrame){
+    if(currentFrame == 0){//if first frame of buffer
+      std::cout << whatToPrint << std::endl;
+    }
+  }
+  //print every Nth buffer
+  //void logBuffer(int buffersToSkip)
+
+  bool firstFrameFlag;//has a new buffer started calculating?
+  bool lastFrameFlag;//has a buffer just finished calculating?
+};
+#endif
---
+++ src/DebugTool.cpp
@@ -0,0 +1,16 @@
+#include "pedal/DebugTool.hpp"
+
+/*
+void DebugTool::update(int currentFrame){
+    //update flags based on buffer position
+    if(currentFrame == 0){
+      firstFrameFlag = true;
+      lastFrameFlag = false;
+    }else if(currentFrame == pdlSettings::bufferSize - 1){
+      firstFrameFlag = false;
+      lastFrameFlag = true;
+    }else{
+      firstFrameFlag = lastFrameFlag = false;
+    }
+}
+*/
