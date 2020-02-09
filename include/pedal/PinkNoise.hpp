0e450db0bfefacf3e5b3e976db878826ee6c197f
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-09 14:41:01 -0800

    clean and ready

--- include/pedal/PinkNoise.hpp
+++ include/pedal/PinkNoise.hpp
@@ -2,38 +2,63 @@
 #define PinkNoise_hpp
 
 #include <iostream>
-#include <vector>
+#include "math.h"
 #include "WhiteNoise.hpp"
+#define NUM_OCTAVES 14 
 
-#define NUM_OCTAVES 12
+class PinkNoise{
+  public:
+  PinkNoise();//constructor
+  ~PinkNoise();//deconstructor
+  float generateSample();//main function
+  float* generateBlock();//fills entire buffer
+  
+  float getSample();//retrieve sample w/o processing
+  float* getBlock();//retrieve block w/o processing
+
+  private:
+  inline int getOctave(int index){//which whitenoise generator
+    int indexLocation = index;//current location within pattern
+    int whichOctave = 0;//octave is 0 by default (right 50% of the time)
+    while(indexLocation % 2 == 1){//if indexLocation is odd
+      whichOctave++;//increment octave
+      indexLocation /= 2;//prepare for next comparison
+    }
+    return whichOctave;//return selected octave
+  }
+  WhiteNoise noiseArray[NUM_OCTAVES];//array of white noise generators
+  unsigned index;//current location in ovtave-retrieval pattern
+  unsigned indexMax;//How many steps in octave-retrieval pattern? (depends on how many octaves)
+  float runningSum;//Sum of all octaves, updated per sample
+  float amplitudeScalar;//ensure the output is between -1.0 and 1.0
+  float currentSample;//store for retrieval
+  float* currentBlock = nullptr;
+  void checkGetOctaveLogic();//Leaving this in for the curious type
+};
+#endif
+
 /*
-//0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 1  | 0
+This pink noise implementation uses an N-octave
+sample-and-hold sum. On each sample, one of the N
+WhiteNoise generators generates a new sample. That
+generator's previous value is first subtracted from 
+a running total, then the new value is added to the
+running total.
+
 //1x x x x x x x x x x x x x x x x 2  | 0
 //2 x   x   x   x   x   x   x   x  4  | 1
 //3   x       x       x       x    8  | 3
 //4       x               x        16 | 7 
 //5               x                32 | 15
+...                                  ...
+//12                             4096 | 2047
+
+No matter how many octaves there are, this 
+pattern can provide a solution with a series
+of steps equal to 2^N. The function .getOctave()
+takes this integer index in the pattern and produces
+the correct octave to change for that time step.
+
+This approach was adopted from techniques found at this url
+http://www.firstpr.com.au/dsp/pink-noise/
 */
-class PinkNoise{
-  public:
-  PinkNoise();
-  float generateSample();
-  
-  private:
-  WhiteNoise noiseArray[NUM_OCTAVES];
-  unsigned index;//
-  unsigned indexMax;
-  float runningSum;
-  float currentSample;
-  void checkGetOctaveLogic();
-  inline int getOctave(int index){
-    int indexLocation = index;
-    int whichOctave = 0;
-    while(indexLocation % 2 == 1){
-      whichOctave++;
-      indexLocation /= 2;
-    }
-    return whichOctave;
-  }
-};
-#endif
--- src/generators/noise/PinkNoise.cpp
+++ src/generators/noise/PinkNoise.cpp
@@ -1,35 +1,51 @@
 #include "pedal/PinkNoise.hpp"
-
-#include "math.h"
+//constructors and deconstructors
+//=========================================================
 PinkNoise::PinkNoise(){
-  indexMax = pow(2, NUM_OCTAVES) - 1;
+  indexMax = pow(2, NUM_OCTAVES) - 1;//size of the octave pattern
   runningSum = 0.0f;
+  amplitudeScalar = 1/(float)NUM_OCTAVES;
   //generate a random value to start for all white noise generators
   for(int i = 0; i < NUM_OCTAVES; i++){
-    runningSum += noiseArray[i].generateSample();
+    runningSum += noiseArray[i].generateSample() * amplitudeScalar;
   }
   index = 0;//where are we in the 'whichOctave' logic?
 }
-
-float PinkNoise::generateSample(){
-  int whichOctave = getOctave(index);
-  index++;
-  if(index >= indexMax){index = 0;}
+PinkNoise::~PinkNoise(){
+  delete[] currentBlock;//free the memory 
+}
+//primary mechanics of class
+//=========================================================
+float PinkNoise::generateSample(){//generate a single sample
+  int whichOctave = getOctave(index);//retrieve octave location (which noise)
+  index++;//increment location within pattern for next call
+  if(index >= indexMax){index = 0;}//wrap index if needed
   //before adding the new noise value, we must subtract the previous
   runningSum -= noiseArray[whichOctave].getSample();
   //now we replace it with the new sample
   runningSum += noiseArray[whichOctave].generateSample();
-  currentSample = runningSum + rangedRandom(-1.0f, 1.0f);
-  return currentSample;
+  currentSample = runningSum * amplitudeScalar;//scale then assign to current sample
+  return currentSample;//return calculated sample
 }
 
-void PinkNoise::checkGetOctaveLogic(){
-  unsigned numberOfSteps = pow(2, NUM_OCTAVES)-1;
-  for(int i = 0; i < numberOfSteps; i++){
-    std::cout << "There are " <<  NUM_OCTAVES << 
+float* PinkNoise::generateBlock(){//generate a block of samples
+  if(currentBlock != nullptr){//if we don't have a local currentBlock yet, 
+    currentBlock = new float[pdlSettings::bufferSize];//create a new array of floats
+  }
+  for(int i = 0; i < pdlSettings::bufferSize; i++){//for every index in buffer
+    currentBlock[i] = generateSample();//generate and assign a sample
+  }
+  return currentBlock;//return full buffer
+}
+
+void PinkNoise::checkGetOctaveLogic(){//function used while designing this class
+  unsigned numberOfSteps = pow(2, NUM_OCTAVES)-1;//number intuited from graph in hpp
+  //print octaves and steps
+  std::cout << "There are " <<  NUM_OCTAVES << 
                  "octaves, requiring " << numberOfSteps << 
                  "steps" << std::endl;
-    int whichOctave = PinkNoise::getOctave(i) + 1;
+  for(int i = 0; i < numberOfSteps; i++){//scan through each step
+    int whichOctave = PinkNoise::getOctave(i);
     std::cout << whichOctave << "|";
     for(int j = 0; j < whichOctave; j++){
       std::cout << "*" ;
@@ -37,3 +53,8 @@ void PinkNoise::checkGetOctaveLogic(){
     std::cout << "\n";
   }
 }
+//Getters and setters
+//=========================================================
+//no setters in this class
+float PinkNoise::getSample(){return currentSample;}
+float* PinkNoise::getBlock(){return currentBlock;}
