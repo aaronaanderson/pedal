8bc6295d547d9ce6bcd4eff5cdd54b45f9b58eaa
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-09 14:50:01 -0800

    commented and cleaned

--- include/pedal/WhiteNoise.hpp
+++ include/pedal/WhiteNoise.hpp
@@ -3,13 +3,18 @@
 
 #include "utilities.hpp"
 
-class WhiteNoise{
+class WhiteNoise{//basic white noise signal generator
   public:
-  WhiteNoise();
-  float generateSample();
-  float getSample();
+  WhiteNoise();//Constructor
+  ~WhiteNoise();//Deconstructor
+  float generateSample();//generate a single sample
+  float* generateBlock();//generate a block of samples
+
+  float getSample();//retrieve a sample w/o advancing
+  float* getBlock();//retrieve a block w/o advancing
 
   private:
-  float currentSample;
+  float currentSample;//store current sample
+  float* currentBlock = nullptr;//store curent buffer 
 };
 #endif
--- src/generators/noise/WhiteNoise.cpp
+++ src/generators/noise/WhiteNoise.cpp
@@ -1,11 +1,30 @@
 #include "pedal/WhiteNoise.hpp"
 
+//constructors and deconstructors
+//=========================================================
 WhiteNoise::WhiteNoise(){
-  currentSample = generateSample();
+  currentSample = generateSample();//start wtih a random sample
+}
+WhiteNoise::~WhiteNoise(){
+  delete[] currentBlock;
 }
 
-float WhiteNoise::generateSample(){
-  currentSample = rangedRandom(-1.0f, 1.0f);
+//primary mechanics of class
+//=========================================================
+float WhiteNoise::generateSample(){//generate a single sample
+  currentSample = rangedRandom(-1.0f, 1.0f);//very basic random....could be more random
+}
+float* WhiteNoise::generateBlock(){//generate a block of samples
+  if(currentBlock != nullptr){//if we don't have a local currentBlock yet, 
+    currentBlock = new float[pdlSettings::bufferSize];//create a new array of floats
+  }
+  for(int i = 0; i < pdlSettings::bufferSize; i++){//for every index in the buffer
+    currentBlock[i] = generateSample();//generate and assign a single sample
+  }
+  return currentBlock;
 }
 
+//Getters and setters
+//=========================================================
 float WhiteNoise::getSample(){return currentSample;}
+float* WhiteNoise::getBlock(){return currentBlock;}
