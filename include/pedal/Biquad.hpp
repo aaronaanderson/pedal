17f1a5f6bddb83d494e372b0ee7737f046348d14
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-09 23:37:43 -0800

    serious start on biquad

--- include/pedal/Biquad.hpp
+++ include/pedal/Biquad.hpp
@@ -1,50 +1,61 @@
 #ifndef Biquad_hpp
 #define Biquad_hpp
 
+#include "math.h"
 //in progress....
-
 
 enum FilterType{
   LOW_PASS = 0,
   HIGH_PASS,
   BAND_PASS,
-  RESONANT,
   BAND_REJECT,
-  ALL_PASS,
-  PEAKING,
+  PEAK,
   LOW_SHELF,
   HIGH_SHELF
 };
 
 class Biquad{
   public:
-  Biquad();
+  Biquad(FilterType initialMode = LOW_PASS, 
+         float initialFrequency = 2000.0f,
+         float initialQ = 2.0f);
   ~Biquad();
 
   inline float processSample(float input);
   float* processBlock(float* input);
 
   void setFrequency(float newFrequency);
-  void setResonance(float newResonance);
-  void setLevel(float newLevel);
+  void setGain(float newGain);
+  void setQ(float newQ);
   void setMode(FilterType newMode);
 
   float getFrequency();
-  float getResonance();
-  float getLevel();
+  float getGain();
+  float getQ();
   FilterType getMode();
-
   float getSample();
   float* getBlock();
 
   private:
-  void calculateCoefficients();
+  void calculateCoefficients();//called when parameters change
   float frequency;//center/cutoff/shelf frequency depending on mode
+  float q;
+  float gain;
   FilterType mode;//enumerated above
   float a0, a1, a2;//feed-forward coefficients
   float b1, b2;//feed-back coefficients
   float z1, z2;//containers for previous output
-  float currentSample;//this could be considered z0 in this case
+  float currentSample;
   float* currentBlock;//storage for processing entire block
 };
+/*
+Transposed Direct Form II, thanks to Nigel Redmon
+https://www.earlevel.com/main/2012/11/26/biquad-c-source-code/
+*/
+inline float Biquad::processSample(float input){
+  currentSample = input * a0 + z1;
+  z1 = input * a1 + z2 - b1 * currentSample;
+  z2 = input * a2 - b2 * currentSample;
+  return currentSample;
+}
 #endif
--- src/modifiers/filters/Biquad.cpp
+++ src/modifiers/filters/Biquad.cpp
@@ -1,29 +1,113 @@
 #include "pedal/Biquad.hpp"
 //constructors and deconstructors
 //=========================================================
-
+Biquad::Biquad(FilterType initialMode, float initialFrequency, float initialQ){
+  frequency = initialFrequency;
+  mode = initialMode;
+  q = initialQ;
+  gain = 0.0f;
+  calculateCoefficients();
+}
 
 //primary mechanics of class
-//=========================================================
+//===============================v
 void Biquad::calculateCoefficients(){
+  float norm;
+  float v = pow(10, fabs(gain)/20.0f);
+  float k = tan(M_PI * frequency);
+  //calculate and store these since used frequently
+  float k_squared = k * k;
+  float kdivbyq = k / q;
   switch(mode){
     case LOW_PASS:
+    norm = 1.0f / (1.0f + kdivbyq + k_squared);
+    a0 = k_squared * norm;
+    a1 = 2.0f * a0;
+    a2 = a0;
+    b1 = 2.0f * (k_squared - 1.0f) * norm;
+    b2 = (1.0f - kdivbyq + k_squared) * norm;
     break;
     case HIGH_PASS:
+    norm = 1.0f / (1.0f + kdivbyq + k_squared);
+    a0 = 1.0f * norm;
+    a1 = -2.0f * a0;
+    a2 = a0;
+    b1 = 2.0f * (k_squared - 1.0f) * norm;
+    b2 = (1.0f - kdivbyq + k_squared) * norm;
     break;
     case BAND_PASS:
-    break;
-    case RESONANT:
+    norm = 1.0f / (1.0f + kdivbyq + k_squared);
+    a0 = norm;
+    a1 = -2.0f * a0;
+    a2 = a0;
+    b1 = 2.0f * (k_squared - 1.0f) * norm;
+    b2 = (1.0f - kdivbyq + k_squared) * norm;
     break;
     case BAND_REJECT:
+    norm = 1.0f / (1.0f / kdivbyq + k_squared);
+    a0 = (1.0f + k_squared) * norm;
+    a1 = 2.0f * (k_squared - 1.0f) * norm;
+    a2 = a0;
+    b1 = a1;
+    b2 = (1.0f - kdivbyq + k_squared) * norm;
     break;
-    case ALL_PASS:
-    break;
-    case PEAKING:
+    case PEAK:
+    a1 = 2.0f * (k_squared - 1.0f) * norm;
+    b1 = a1;
+    float vqk = v / q * k;//calculate and store, since used often
+    if(gain > 0.0f){
+      norm = 1.0f / (1.0f + 1.0f / q * k + k_squared);
+      a0 = (1.0f + vqk + k_squared) * norm;
+      a2 = (1.0f - vqk + k_squared) * norm;
+      b2 = (1.0f - 1.0f / q * k + k_squared) * norm;
+    }else{
+      norm = 1.0f / (1.0f + vqk + k_squared);
+      a0 = (1.0f + 1.0f/q * k + k_squared) * norm;
+      a2 = (1.0f - 1.0f/q * k + k_squared) * norm;
+      b2 = (1.0f - vqk + k_squared) * norm;
+    }
     break;
     case LOW_SHELF:
+    {
+    float sqrt2k = sqrt(2.0f)*k;//calculate and store, since used often
+    float sqrt2vk = sqrt(2.0f * v) * k;
+    if(gain >= 0.0f){
+      norm = 1.0f / (1.0f + sqrt2k + k_squared);
+      a0 = (1.0f + sqrt2vk + v * k_squared)  * norm;
+      a1 = 2.0f * (v * k_squared - v) * norm;
+      a2 = (1.0f - sqrt2vk + v * k_squared) * norm;
+      b1 = 2.0f * (k_squared - 1.0f) * norm;
+      b2 = (1.0f - sqrt2k + k_squared) * norm;
+    }else{
+      norm = 1.0f / (1.0f + sqrt2vk + v * k_squared);
+      a0 = (1.0f + sqrt2k + k_squared) * norm;
+      a1 = 2.0f * (k_squared - 1.0f) * norm;
+      a2 = (1.0f - sqrt2k + k_squared) * norm;
+      b1 = 2.0f * (v * k_squared - 1.0f) * norm;
+      b2 = (1.0f - sqrt2vk + k_squared) * norm;
+    }
+    }
     break;
     case HIGH_SHELF:
+    {
+    float sqrt2k = sqrt(2.0f)*k;//calculate and store, since used often
+    float sqrt2vk = sqrt(2.0f * v) * k;
+    if(gain >= 0.0f){
+      norm = 1.0f / (1.0f + sqrt2k + k_squared);
+      a0 = (v + sqrt2vk + k_squared) * norm;
+      a1 = 2.0f * (k_squared - v) * norm;
+      a2 = (v - sqrt2vk + k_squared) * norm;
+      b1 = 2.0f * (k_squared - 1.0f) * norm;
+      b2 = (1.0f - sqrt2k + k_squared) * norm;
+    }else{
+      norm = 1.0f / (v + sqrt2vk + k_squared);
+      a0 = (1.0f + sqrt2vk + k_squared) * norm;
+      a1 = 2.0f * (k_squared - 1.0f) * norm;
+      a2 = (1.0f - sqrt2k + k_squared) * norm;
+      b1 = 2.0f * (k_squared - v) * norm;
+      b2 = (v - sqrt2vk + k_squared) * norm;
+    }
+    }
     break; 
   }
 }
