50b3b024c04fa5a7a27acdae26cf420a21bcc79e
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-11 17:19:24 -0800

    fixed bug in biquad

--- src/modifiers/filters/Biquad.cpp
+++ src/modifiers/filters/Biquad.cpp
@@ -26,59 +26,59 @@ float* Biquad::processBlock(float* input){
 }
 void Biquad::calculateCoefficients(){
   double norm;
-  double v = pow(10, fabs(gain)/20.0f);
+  double v = pow(10, fabs(gain)/20.0);
   double k = tan(M_PI * frequency);
   //calculate and store these since used frequently
   double k_squared = k * k;
   double kdivbyq = k / q;
   switch(mode){
-    case LOW_PASS:
-    norm = 1.0f / (1.0f + kdivbyq + k_squared);
+    case LOW_PASS:https://www.youtube.com/watch?v=GmBHwjoIFNM
+    norm = 1.0 / (1.0 + kdivbyq + k_squared);
     a0 = k_squared * norm;
-    a1 = 2.0f * a0;
+    a1 = 2.0 * a0;
     a2 = a0;
-    b1 = 2.0f * (k_squared - 1.0f) * norm;
-    b2 = (1.0f - kdivbyq + k_squared) * norm;
+    b1 = 2.0 * (k_squared - 1.0) * norm;
+    b2 = (1.0 - kdivbyq + k_squared) * norm;
     break;
     case HIGH_PASS:
-    norm = 1.0f / (1.0f + kdivbyq + k_squared);
-    a0 = 1.0f * norm;
-    a1 = -2.0f * a0;
+    norm = 1.0 / (1.0 + kdivbyq + k_squared);
+    a0 = 1.0 * norm;
+    a1 = -2.0 * a0;
     a2 = a0;
-    b1 = 2.0f * (k_squared - 1.0f) * norm;
-    b2 = (1.0f - kdivbyq + k_squared) * norm;
+    b1 = 2.0 * (k_squared - 1.0) * norm;
+    b2 = (1.0 - kdivbyq + k_squared) * norm;
     break;
     case BAND_PASS:
-    norm = 1.0f / (1.0f + kdivbyq + k_squared);
+    norm = 1.0 / (1.0 + kdivbyq + k_squared);
     a0 = k / q * norm;
-    a1 = 0.0f;
+    a1 = 0.0;
     a2 = -a0;
-    b1 = 2.0f * (k_squared - 1.0f) * norm;
-    b2 = (1.0f - kdivbyq + k_squared) * norm;
+    b1 = 2.0 * (k_squared - 1.0) * norm;
+    b2 = (1.0 - kdivbyq + k_squared) * norm;
     break;
     case BAND_REJECT:
-    norm = 1.0f / (1.0f / kdivbyq + k_squared);
-    a0 = (1.0f + k_squared) * norm;
-    a1 = 2.0f * (k_squared - 1.0f) * norm;
+    norm = 1.0 / (1.0 + kdivbyq + k_squared);
+    a0 = (1.0 + k_squared) * norm;
+    a1 = 2.0 * (k_squared - 1.0) * norm;
     a2 = a0;
     b1 = a1;
-    b2 = (1.0f - kdivbyq + k_squared) * norm;
+    b2 = (1.0 - kdivbyq + k_squared) * norm;
     break;
     case PEAK:
     {
-    a1 = 2.0f * (k_squared - 1.0f) * norm;
+    a1 = 2.0 * (k_squared - 1.0) * norm;
     b1 = a1;
     double vqk = v / q * k;//calculate and store, since used often
-    if(gain > 0.0f){
-      norm = 1.0f / (1.0f + 1.0f / q * k + k_squared);
-      a0 = (1.0f + vqk + k_squared) * norm;
-      a2 = (1.0f - vqk + k_squared) * norm;
-      b2 = (1.0f - 1.0f / q * k + k_squared) * norm;
+    if(gain >= 0.0f){
+      norm = 1.0 / (1.0 + 1.0 / q * k + k_squared);
+      a0 = (1.0 + v / q * k + k_squared) * norm;
+      a2 = (1.0 - v / q * k + k_squared) * norm;
+      b2 = (1.0 - 1.0 / q * k + k_squared) * norm;
     }else{
-      norm = 1.0f / (1.0f + vqk + k_squared);
-      a0 = (1.0f + 1.0f/q * k + k_squared) * norm;
-      a2 = (1.0f - 1.0f/q * k + k_squared) * norm;
-      b2 = (1.0f - vqk + k_squared) * norm;
+      norm = 1.0 / (1.0 + vqk + k_squared);
+      a0 = (1.0 + 1.0 / q * k + k_squared) * norm;
+      a2 = (1.0 - 1.0 / q * k + k_squared) * norm;
+      b2 = (1.0 - v / q * k + k_squared) * norm;
     }
     }
     break;
@@ -86,40 +86,40 @@ void Biquad::calculateCoefficients(){
     {
     double sqrt2k = sqrt(2.0f)*k;//calculate and store, since used often
     double sqrt2vk = sqrt(2.0f * v) * k;
-    if(gain >= 0.0f){
-      norm = 1.0f / (1.0f + sqrt2k + k_squared);
-      a0 = (1.0f + sqrt2vk + v * k_squared)  * norm;
-      a1 = 2.0f * (v * k_squared - 1.0f) * norm;
-      a2 = (1.0f - sqrt2vk + v * k_squared) * norm;
-      b1 = 2.0f * (k_squared - 1.0f) * norm;
-      b2 = (1.0f - sqrt2k + k_squared) * norm;
+    if(gain >= 0.0){
+      norm = 1.0 / (1.0 + sqrt2k + k_squared);
+      a0 = (1.0 + sqrt2vk + v * k_squared)  * norm;
+      a1 = 2.0 * (v * k_squared - 1.0) * norm;
+      a2 = (1.0 - sqrt2vk + v * k_squared) * norm;
+      b1 = 2.0 * (k_squared - 1.0) * norm;
+      b2 = (1.0 - sqrt2k + k_squared) * norm;
     }else{
-      norm = 1.0f / (1.0f + sqrt2vk + v * k_squared);
-      a0 = (1.0f + sqrt2k + k_squared) * norm;
-      a1 = 2.0f * (k_squared - 1.0f) * norm;
-      a2 = (1.0f - sqrt2k + k_squared) * norm;
-      b1 = 2.0f * (v * k_squared - 1.0f) * norm;
-      b2 = (1.0f - sqrt2vk + v * k_squared) * norm;
+      norm = 1.0 / (1.0 + sqrt2vk + v * k_squared);
+      a0 = (1.0 + sqrt2k + k_squared) * norm;
+      a1 = 2.0 * (k_squared - 1.0) * norm;
+      a2 = (1.0 - sqrt2k + k_squared) * norm;
+      b1 = 2.0 * (v * k_squared - 1.0) * norm;
+      b2 = (1.0 - sqrt2vk + v * k_squared) * norm;
     }
     }
     break;
     case HIGH_SHELF:
     {
-    float sqrt2k = sqrt(2.0f)*k;//calculate and store, since used often
-    float sqrt2vk = sqrt(2.0f * v) * k;
+    float sqrt2k = sqrt(2.0)*k;//calculate and store, since used often
+    float sqrt2vk = sqrt(2.0 * v) * k;
     if(gain >= 0.0f){
-      norm = 1.0f / (1.0f + sqrt2k + k_squared);
+      norm = 1.0 / (1.0 + sqrt2k + k_squared);
       a0 = (v + sqrt2vk + k_squared) * norm;
-      a1 = 2.0f * (k_squared - v) * norm;
+      a1 = 2.0 * (k_squared - v) * norm;
       a2 = (v - sqrt2vk + k_squared) * norm;
-      b1 = 2.0f * (k_squared - 1.0f) * norm;
-      b2 = (1.0f - sqrt2k + k_squared) * norm;
+      b1 = 2.0 * (k_squared - 1.0) * norm;
+      b2 = (1.0 - sqrt2k + k_squared) * norm;
     }else{
-      norm = 1.0f / (v + sqrt2vk + k_squared);
-      a0 = (1.0f + sqrt2vk + k_squared) * norm;
-      a1 = 2.0f * (k_squared - 1.0f) * norm;
-      a2 = (1.0f - sqrt2k + k_squared) * norm;
-      b1 = 2.0f * (k_squared - v) * norm;
+      norm = 1.0 / (v + sqrt2vk + k_squared);
+      a0 = (1.0 + sqrt2vk + k_squared) * norm;
+      a1 = 2.0 * (k_squared - 1.0) * norm;
+      a2 = (1.0 - sqrt2k + k_squared) * norm;
+      b1 = 2.0 * (k_squared - v) * norm;
       b2 = (v - sqrt2vk + k_squared) * norm;
     }
     }
@@ -127,7 +127,7 @@ void Biquad::calculateCoefficients(){
   }
 }
 void Biquad::flush(){
-  z1 = z2 = 0.0f;
+  z1 = z2 = 0.0;
 }
 //Getters and setters
 //=========================================================
