77b7a72aae61c0481d469b584edb03a5a1ac5c99
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-17 09:13:42 -0800

    added main functionality

---
+++ include/pedal/CombFilter.hpp
@@ -0,0 +1,40 @@
+#ifndef CombFilter_hpp
+#define CombFilter_hpp
+
+#include "CircularBuffer.hpp"
+
+/* Filter Diagram
+         -->[DELAY1]*a   b*[DELAY2]<--
+        /             \ /             \
+input__/_______________+_______________\__output
+a = feedForwardGain, b = feedBackGain
+DELAY1 = inputDelayBuffer
+DELAY2 = outputDelayBuffer
+*/
+class CombFilter{
+  public:
+  CombFilter();
+  
+  float filter(float input);
+  
+  float getFeedForwardDelayTime();
+  float getFeedBackDelayTime();
+  float getFeedBackGain();
+  float getFeedForwardGain();
+  void setFeedForwardDelayTime(float newFFDelayTime);
+  void setFeedBackDelayTime(float newFBDelayTime);
+  void setFeedBackGain(float newFBGain);
+  void setFeedForwardGain(float newFFGain);
+
+  private:
+  float currentSample;
+  CircularBuffer inputDelayBuffer;
+  CircularBuffer outputDelayBuffer;
+  float feedForwardDelayTime;
+  float feedBackDelayTime;
+  float delayTime;
+  float maxDelayTime = 1000.0f;
+  float feedForwardGain;
+  float feedBackGain;
+};
+#endif 
--- src/modifiers/filters/CombFilter.cpp
+++ src/modifiers/filters/CombFilter.cpp
@@ -1,1 +1,36 @@
 #include "pedal/CombFilter.hpp"
+
+//Constructors and deconstructors=====================
+CombFilter::CombFilter(){
+  inputDelayBuffer.setDuration(1000.0f);
+  outputDelayBuffer.setDuration(1000.0f);
+}
+
+//Core functionality of class=========================
+float CombFilter::filter(float input){
+  inputDelayBuffer.inputSample(input);
+  float feedForwardSample = inputDelayBuffer.getDelayed(feedForwardDelayTime);
+  currentSample = feedForwardSample;
+  currentSample += outputDelayBuffer.getDelayed(feedBackDelayTime);
+  outputDelayBuffer.inputSample(currentSample);
+  return currentSample;
+}
+//getters and setters================================
+float CombFilter::getFeedForwardDelayTime(){return feedForwardDelayTime;}
+float CombFilter::getFeedBackDelayTime(){return feedBackDelayTime;}
+float CombFilter::getFeedBackGain(){return feedBackGain;}
+float CombFilter::getFeedForwardGain(){return feedForwardGain;}
+void CombFilter::setFeedForwardDelayTime(float newFFDelayTime){
+  newFFDelayTime = clamp(newFFDelayTime, 0.0f, inputDelayBuffer.getDuration());
+  feedForwardDelayTime = newFFDelayTime;
+}
+void CombFilter::setFeedBackDelayTime(float newFBDelayTime){
+  newFBDelayTime = clamp(newFBDelayTime, 0.0f, outputDelayBuffer.getDuration);
+  feedBackDelayTime =  newFBDelayTime;
+}
+void CombFilter::setFeedBackGain(float newFBGain){
+  feedBackGain = clamp(newFBGain, 0.0f, 1.0f);
+}
+void CombFilter::setFeedForwardGain(float newFFGain){
+  feedForwardGain = clamp(newFFGain, 0.0f, 1.0f);
+}
