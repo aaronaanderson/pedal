e1b76ecc93c13befa351bf2ceed5ddd13d6dee82
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-16 17:46:29 -0800

    added BufferPlayer main functionality

--- examples/BufferPlayground.cpp
+++ examples/BufferPlayground.cpp
@@ -6,12 +6,13 @@
 #include "pedal/Buffer.hpp"
 //#include <iostream>
 //#include "pedal/DebugTool.hpp"
-#include "AudioFFT.h"
+//#include "AudioFFT.h"
+#include "pedal/BufferPlayer.hpp"
 
 //DebugTool debugger;
 Buffer testBuffer(4000.0f);//Initiate buffer with 10 seconds duration
 //testBuffer.fillSineSweep();//breaks
-int bufferIndex = 0;
+BufferPlayer player(&testBuffer);
 //========================Audio Callback
 void callback(float* out, unsigned buffer, unsigned rate, unsigned channel,
               double time, pdlExampleApp* app) {
@@ -21,17 +22,15 @@ void callback(float* out, unsigned buffer, unsigned rate, unsigned channel,
   if(writeFile){
  //   testBuffer.writeSoundFile("temp");
   }
+  player.setSpeed(pdlGetSlider(app, 0));
   for (unsigned i = 0; i < buffer; i += 1) {//for entire buffer of frames
     //DebugTool::printOncePerBuffer(oscillator.getFrequency(), i);
     float leftSample = 0.0f;
     float rightSample = 0.0f;
-    if(bufferIndex > testBuffer.getDurationInSamples()-1){
-      bufferIndex = 0;
-    }else{
-      leftSample = testBuffer.getSample(bufferIndex, 1);
-      rightSample = testBuffer.getSample(bufferIndex, 2);
-      bufferIndex++;
-    }
+    player.update();
+    leftSample = player.getSample(0);
+    rightSample = player.getSample(1);
+  
     out[channel * i] = leftSample;
     out[channel * i + 1] = rightSample;
   }
@@ -50,7 +49,7 @@ int main() {
     //make an app (a pointer to an app, actually)
     //testBuffer.writeSoundFile("temp");//danger!
     // Add your GUI elements here
-    //pdlAddSlider(app, 0, "frequency", 0.1f, 40.0f, 3.0f);
+    pdlAddSlider(app, 0, "Speed", -2.0f, 16.0f, 1.0f);
     //pdlAddToggle(app, 0, "loud", false);
     pdlAddTrigger(app, 0, "trigger");
     
--- include/pedal/BufferPlayer.hpp
+++ include/pedal/BufferPlayer.hpp
@@ -1,9 +1,39 @@
 #ifndef BufferPlayer_hpp
 #define BufferPlayer_hpp
 
+#include "Buffer.hpp"
+
+enum interpolation{
+  NONE = 0, 
+  LINEAR, 
+  CUBIC
+};
+
+enum playMode{
+  ONE_SHOT = 0, 
+  LOOP, 
+  PING_PONG
+  //TODO LOOP_DUCKING 
+};
+
 class BufferPlayer{
   public:
+  BufferPlayer(Buffer* reference = nullptr);
+  
+  float update();
+  
+  
+  void setSpeed(float newSpeed);
+  float getSample(int channel = 0);
+  float* getFrame();
 
   private:
+  float* currentFrame = nullptr;
+  void assignDataFromReference(Buffer* reference);
+  float index;
+  unsigned totalSampleCount;
+  int numberChannels;
+  float playSpeed;
+  Buffer* bufferReference;
 };
 #endif 
