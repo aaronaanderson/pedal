f9da5affd38d6fd7e49de88441bfb73b7d4900b7
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-17 11:32:53 -0800

    added most allpass functionality

--- include/pedal/AllPass.hpp
+++ include/pedal/AllPass.hpp
@@ -1,9 +1,32 @@
 #ifndef AllPass_hpp
 #define AllPass_hpp
 
+#include "CircularBuffer.hpp"
+
+//Diagram based on https://www.uaudio.com/blog/allpass-filters/
+//         --->*a-----
+//        /           \
+//input__/___[DELAY]___\__output 
+//         \            /
+//          \          /
+//           -*(-a)<---
+
 class AllPass{
   public:
+  AllPass();
+
+  float filter(float input);
+  
+  float getDelayTime();
+  float getMaxDelay();
+  void setDelayTime(float newDelayTime);
+  void setMaxDelay(float newMaxDelay);
 
   private:
+  float currentSample;
+  CircularBuffer delayLine;
+  float delayTime;
+  float maxDelay = 2000.0f;
+  float coefficient;
 };
 #endif 
--- src/modifiers/filters/AllPass.cpp
+++ src/modifiers/filters/AllPass.cpp
@@ -1,2 +1,31 @@
 #include "pedal/AllPass.hpp"
 
+AllPass::AllPass(){
+  delayLine.setDuration(maxDelay);
+  coefficient = 0.7f;
+  delayTime = 10.0f;
+  previousSample = 0.0f;
+}
+
+float AllPass::filter(float input){
+  delayLine.inputSample(input + previousSample);
+  currentSample = delayLine.getDelayed(delayTime);
+  currentSample += input * coefficient;
+  previousSample = currentSample * -coefficient;
+  return currentSample;
+}
+
+float AllPass::getDelayTime(){return delayTime;}
+float AllPass::getMaxDelay(){return maxDelay;}
+float AllPass::getCoefficient(){return coefficient;}
+void AllPass::setDelayTime(float newDelayTime){
+  if(newDelayTime < maxDelay){
+    newDelayTime = delayTime;
+  }
+}
+void AllPass::setMaxDelay(float newMaxDelay){
+  if(newMaxDelay > 0.0f){
+    delayLine.setDuration(newMaxDelay);
+  }
+}
+void AllPass::setCoefficient(float newCoefficient){coefficient = newCoefficient;}
