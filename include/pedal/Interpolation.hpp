27a3dbf2295b8124bb573fb1aacc026221110e87
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-16 19:39:31 -0800

    added cubic interp

--- include/pedal/Interpolation.hpp
+++ include/pedal/Interpolation.hpp
@@ -1,6 +1,7 @@
 #ifndef Interpolation_hpp
 #define Interpolation_hpp
 
+#include "math.h"
 //the least expensive, yet still effective option for interpolation
 inline float linearInterpolation(float inputSample, float previousSample, float nextSample){
   //determine position between previous and next sample
@@ -8,4 +9,8 @@ inline float linearInterpolation(float inputSample, float previousSample, float 
   //the result is a weighted sum based on this position
   return (previousSample * (1.0f-position)) + (nextSample * position);
 }
+
+float cubicInterpolation(float inputSample, float backTwo, float backOne, 
+                         float forwardOne, float forwardTwo);
+
 #endif
--- src/Interpolation.cpp
+++ src/Interpolation.cpp
@@ -1,6 +1,13 @@
 #include "pedal/Interpolation.hpp"
 
 
-/*
-functions are inlined, so in the header
-*/
+float cubicInterpolation(float inputSample, float backTwo, float backOne, 
+                         float forwardOne, float forwardTwo){
+    float x = inputSample - int(inputSample); // how far between points x1 and x2?
+    float a = -0.5*backTwo + 1.5*backOne - 1.5*forwardOne + 0.5 * forwardTwo;
+    float b = backTwo - 2.5*backOne + 2*forwardOne - 0.5*forwardTwo;
+    float c = -0.5*backTwo + 0.5*forwardOne;
+    float d = backOne;
+    return (a*pow(x, 3)) + (b*pow(x, 2)) + (c*x) + d;
+};
+
