b36efba64a39761810c321e214877be5ee01c614
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-19 21:20:21 -0800

    major start

--- include/pedal/MoorerReverb.hpp
+++ include/pedal/MoorerReverb.hpp
@@ -1,9 +1,46 @@
 #ifndef MoorerReverb_hpp
 #define MoorerReverb_hpp
 
+#include "pedal/LowPassCombFilter.hpp"
+#include "pedal/AllPass.hpp"
+#include "pedal/CircularBuffer.hpp"
+#include "pedal/utilities.hpp"
+#define NUM_COMBS 6
+#define NUM_TAPS 16
+
 class MoorerReverb{
   public:
+  MoorerReverb();
+
+  float process(float input);
+  float getSample();
+  float setReverbTime(float newReverbTime);//not safe for audio thread
+  float setDryWet(float newDryWet);
 
   private:
+  float currentSample;
+  float time;//time to -60dBFS
+  float dryWetMix;
+  AllPass allPass;
+  CircularBuffer delayLine;//buffer for multi-tap delay (early reflections)
+  const float delayTimes[NUM_TAPS] = {4.3f, 21.5f, 22.5f, 26.8f,
+                                27.0f, 29.8f, 45.8f, 48.5f, 
+                                57.2f, 58.7f, 59.55f, 61.2f, 
+                                70.7, 70.8f, 72.6f, 74.1f, 
+                                75.3f, 79.7f};
+  const float delayGains[NUM_TAPS] = {0.841f, 0.504f, 0.491f, 0.379f, 
+                                0.380f, 0.346f, 0.289f, 0.272f, 
+                                0.192f, 0.192f, 0.217f, 0.181f, 
+                                0.180f, 0.181f, 0.176f, 0.142f, 
+                                0.167f, 0.134f};
+  LowPassCombFilter filterBank[NUM_COMBS];//an array of lowpass comb filters
+  const float combDelayTimess[NUM_COMBS] = {50.0f, 56.0f, 61.0f,
+                                          68.0f, 72.0f, 78.0f};
+  const float combFilterFrequencies[NUM_COMBS] = {1942.0f, 1363.0f, 
+                                                 1312.0f, 1574.0f, 
+                                                 981.0f, 1036.0f};
+  //TODO Make this a AoS instead of SoA
+
+    
 };
 #endif
--- src/modifiers/reverb/MoorerReverb.cpp
+++ src/modifiers/reverb/MoorerReverb.cpp
@@ -1,1 +1,28 @@
 #include "MoorerReverb.hpp"
+MoorerReverb::MoorerReverb(){
+  //initiate Low Pass Comb Filters
+  for(int i = 0; i < NUM_COMBS; i++){
+    filterBank[i].setDelayTime(combDelayTimess[i]);
+    filterBank[i].setFilterFrequency(combFilterFrequencies[i]);
+  }
+}
+
+//============================================
+float MoorerReverb::process(float input){
+  currentSample = 0.0f;
+  delayLine.inputSample(input);//feed the delay line
+  //handle th early reflections
+  for(int i = 0; i < NUM_TAPS; i++){//for every tap (of multitap delay)
+    currentSample += delayLine.getDelayed(delayTimes[i]) * //check array for time
+                     delayGains[i];//scale buffer read by gain
+  }
+  //feed every comb filter the same input (because parallel)
+  float filterBankSum = 0.0f;//save cpu cycles by collecting this as we feed the filters
+  for(int i = 0; i < NUM_COMBS; i++){//for every comb filter
+    filterBankSum += filterBank[i].process(currentSample);//each filter gets the same input
+  }
+  //next is allpass
+  currentSample = allPass.process(filterBankSum);
+  currentSample = (currentSample * dryWetMix) + (input * (1.0f-dryWetMix));
+  return currentSample;
+}
