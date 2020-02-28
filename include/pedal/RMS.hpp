829424b1ff9e5db70cbdde7e34686fc98598a45f
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-28 13:56:07 -0800

    more accurate and less expensive

--- include/pedal/RMS.hpp
+++ include/pedal/RMS.hpp
@@ -3,19 +3,36 @@
 
 #include <algorithm>
 #include "utilities.hpp"
+#include "Buffer.hpp"
 //Root Mean Squared
 class RMS{
   public:
   RMS(int samplePeriod = 64);
-  float process(float input);
+  inline float process(float input);
   void setSamplePeriod(int newSamplePeriod);//how many samples to average
   int getSamplePeriod();
   float getSample();
   
   private:
-  SmoothValue<float> currentSample;//output only calculated every N samples, so smooth
+  Buffer sampleBuffer;
+  int writeIndex;
+  float periodReciprocal;//store
+  float currentSample;//output only calculated every N samples, so smooth
   int samplesToAverage;//how many samples in an average?
   int sampleCounter;//to keep track of how many we've added
   float runningTotal;
 };
+
+inline float RMS::process(float input){
+  sampleCounter%samplesToAverage;//wrap the index
+  //subtract what WAS in the buffer first
+  runningTotal -= sampleBuffer.getSample(writeIndex);
+  //add its replacement to the total, then to buffer
+  input = fabs(input);//we want distance from 0
+  runningTotal += input;//add the input
+  sampleBuffer.writeSample(input, writeIndex);
+  writeIndex++;
+  currentSample = runningTotal * periodReciprocal;
+  return currentSample;//return smoothed value
+}
 #endif
--- src/RMS.cpp
+++ src/RMS.cpp
@@ -4,27 +4,17 @@ RMS::RMS(int samplePeriod){//how many samples to average
   samplesToAverage = std::min(samplePeriod, 1);
   sampleCounter = 0;
   runningTotal = 0.0f;//c
+  writeIndex = 0;
+  periodReciprocal = 1.0f / (float)samplesToAverage;
   //set the smoother time to sample period
-  currentSample.setTime(samplesToMS(samplesToAverage));
 }
 
-float RMS::process(float input){
-  sampleCounter%samplesToAverage;//wrap the index
-  if(sampleCounter==0){//if a full buffer has been added
-    runningTotal /= (float)samplesToAverage;
-    currentSample.setTarget(runningTotal);
-    runningTotal = 0.0f;//clear the running total
-  }
-  runningTotal += fabs(input);//collect all the |input|
-  sampleCounter++;//increment counter
-  currentSample.process();//progress the smoother
-  return currentSample.getCurrentValue();//return smoothed value
-}
+
 void RMS::setSamplePeriod(int newSamplePeriod){
   samplesToAverage = std::min(newSamplePeriod, 1);
   sampleCounter = 0;//reset the counter
   runningTotal = 0.0f;//erase the running total
-  currentSample.setTime(samplesToMS(samplesToAverage));
+  sampleBuffer.setDurationInSamples(samplesToAverage);
 }
 int RMS::getSamplePeriod(){return samplesToAverage;}
-float RMS::getSample(){return currentSample.getCurrentValue();}
+float RMS::getSample(){return currentSample;}
