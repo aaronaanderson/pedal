0dce78008f8470b82b324b7efae1988b96894271
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-28 12:59:26 -0800

    RMS ready for testing

--- include/pedal/RMS.hpp
+++ include/pedal/RMS.hpp
@@ -1,18 +1,21 @@
 #ifndef RMS_HPP
 #define RMS_HPP
 
-#include "CircularBuffer.hpp"
+#include <algorithm>
 #include "utilities.hpp"
-
+//Root Mean Squared
 class RMS{
   public:
   RMS(int samplePeriod = 64);
   float process(float input);
+  void setSamplePeriod(int newSamplePeriod);//how many samples to average
+  int getSamplePeriod();
+  float getSample();
+  
   private:
-  CircularBuffer buffer;//keep track of data to average
-  SmoothValue currentSample;//output only calculated every N samples, so smooth
+  SmoothValue<float> currentSample;//output only calculated every N samples, so smooth
   int samplesToAverage;//how many samples in an average?
   int sampleCounter;//to keep track of how many we've added
-
+  float runningTotal;
 };
 #endif
--- src/RMS.cpp
+++ src/RMS.cpp
@@ -1,7 +1,7 @@
 #include "pedal/RMS.hpp"
 
 RMS::RMS(int samplePeriod){//how many samples to average
-  samplesToAverage = fmin(samplePeriod, 1);
+  samplesToAverage = std::min(samplePeriod, 1);
   sampleCounter = 0;
   runningTotal = 0.0f;//c
   //set the smoother time to sample period
@@ -9,7 +9,7 @@ RMS::RMS(int samplePeriod){//how many samples to average
 }
 
 float RMS::process(float input){
-  sampleCounter%samplesToAverage;
+  sampleCounter%samplesToAverage;//wrap the index
   if(sampleCounter==0){//if a full buffer has been added
     runningTotal /= (float)samplesToAverage;
     currentSample.setTarget(runningTotal);
@@ -18,6 +18,13 @@ float RMS::process(float input){
   runningTotal += fabs(input);//collect all the |input|
   sampleCounter++;//increment counter
   currentSample.process();//progress the smoother
-  return currentSample.getCurrentValue();
+  return currentSample.getCurrentValue();//return smoothed value
 }
+void RMS::setSamplePeriod(int newSamplePeriod){
+  samplesToAverage = std::min(newSamplePeriod, 1);
+  sampleCounter = 0;//reset the counter
+  runningTotal = 0.0f;//erase the running total
+  currentSample.setTime(samplesToMS(samplesToAverage));
+}
+int RMS::getSamplePeriod(){return samplesToAverage;}
 float RMS::getSample(){return currentSample.getCurrentValue();}
