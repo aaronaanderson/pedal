7d22da268e01fbac0273fea7da5d990a3aa54368
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-28 12:35:56 -0800

    initial RMS

--- include/pedal/RMS.hpp
+++ include/pedal/RMS.hpp
@@ -1,9 +1,18 @@
 #ifndef RMS_HPP
 #define RMS_HPP
 
+#include "CircularBuffer.hpp"
+#include "utilities.hpp"
+
 class RMS{
   public:
-
+  RMS(int samplePeriod = 64);
+  float process(float input);
   private:
+  CircularBuffer buffer;//keep track of data to average
+  SmoothValue currentSample;//output only calculated every N samples, so smooth
+  int samplesToAverage;//how many samples in an average?
+  int sampleCounter;//to keep track of how many we've added
+
 };
 #endif
--- src/RMS.cpp
+++ src/RMS.cpp
@@ -1,1 +1,23 @@
 #include "pedal/RMS.hpp"
+
+RMS::RMS(int samplePeriod){//how many samples to average
+  samplesToAverage = fmin(samplePeriod, 1);
+  sampleCounter = 0;
+  runningTotal = 0.0f;//c
+  //set the smoother time to sample period
+  currentSample.setTime(samplesToMS(samplesToAverage));
+}
+
+float RMS::process(float input){
+  sampleCounter%samplesToAverage;
+  if(sampleCounter==0){//if a full buffer has been added
+    runningTotal /= (float)samplesToAverage;
+    currentSample.setTarget(runningTotal);
+    runningTotal = 0.0f;//clear the running total
+  }
+  runningTotal += fabs(input);//collect all the |input|
+  sampleCounter++;//increment counter
+  currentSample.process();//progress the smoother
+  return currentSample.getCurrentValue();
+}
+float RMS::getSample(){return currentSample.getCurrentValue();}
