9fa5e4bda2184a4aac4e900ec57a64194ea71481
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-03-17 17:46:20 -0700

    added gate

---
+++ include/pedal/Gate.hpp
@@ -0,0 +1,66 @@
+#ifndef Gate_hpp
+#define Gate_hpp
+
+#include "pedal/CircularBuffer.hpp"
+#include "pedal/BufferedRMS.hpp"
+#include "pedal/utilities.hpp"
+/*
+A gate makes quiet sounds more quiet. How much more
+quiet depends on how high the ratio is, and how quiet
+the input was to begin with. 
+A gate is a special case of an expander; it is a downward 
+expander with a high ratio. Typically these are used to 
+reduce microphone bleed or reduce noise from a signal. 
+*/
+class Gate{
+  public:
+  Gate();
+  float process(float input);
+  float process(float input, float sideChain);
+
+  void setThresholdDB(float newThresholdDB);//At what intensity does attenuation begin?
+  void setRatio(float newRatio);//set attenuation ratio
+  void setRangeDB(float newRangeDB);
+  void setInputGainDB(float inputGainDB);//set input gain in dB
+  void setInputGainLinear(float inputGainLinear);//set by scalar
+  void setMakeUpGainDB(float newMakeUpGainDB);
+  void setAttackTime(float newAttackTime);//set attack time (ms)
+  void setHoldTime(float newHoldTime);//how long to wait after attack and before release
+  void setReleaseTime(float newDecayTime);//set release time (ms)
+  void setLookAheadTime(float newLookAhead);//set look-ahead time in ms
+  void setAnalysisTime(float newAnalysisTime);//16 sample default
+  
+  float getSample();
+  float getLinearScalar();//get final smoothed scalar value
+  float getThresholdDB();
+  float getRatio();//
+  float getRangeDB();//immediate dropoff amount before slope
+  float getInputGainDB();//get inputGain in dB
+  float getMakeUpGainDB();//get makeUpGain in dB
+  float getAttackTime();//(ms)
+  float getReleaseTime();//(ms)
+  float getLookAheadTime();//(ms)
+  float getAnalysisTime();//(ms)
+  
+  private:
+  void updateGain(float input);
+  float currentSample;//store current working sample
+  CircularBuffer delayLine;//analysis takes time, compensate output
+  BufferedRMS signalEstimator;//somewhat expensive amplitude follower
+  float linearThreshold;//store a copy as a scalar
+  float threshold;//stored in dB
+  float ratio;//how many dB of reduction per dB over threshold
+  float rangeDB;//how many dB to drop before slope
+  float linearInputGain;//what value to multiply input by for signal estimation
+  bool attackFlag;// keep track of state (attack or release)
+  DirectionalSmoothValue<float> linearGain;//special SmoothValue that has different arrival times for up or down
+  int attackPhaseInSamples;//current location in attack phase
+  int attackTimeInSamples;//cheaper to store int location
+  int holdTimeInSamples;//how many samples should the hold phase last
+  float lowestAttackPhaseTarget;//keep a running minimum for attack/hold phase
+  float analysisTime;//how long does signalEstimator take?
+  float lookAhead;//push analysis further ahead of 
+  float linearMakeUpGain;//how much to scale the output
+  float reductionTargetDB;//how much reduction is being attempted
+};
+#endif
---
+++ src/modifiers/dynamic/Gate.cpp
@@ -0,0 +1,113 @@
+#include "pedal/Gate.hpp"
+
+Gate::Gate(){
+  delayLine.setDuration(100.0f);//won't need much
+  setThresholdDB(-50.0f);//at what intensity should the compressor start compressing?
+  setRangeDB(0.0f);
+  setRatio(20.0f);//if over threshold by 'ratio' decibels, scale down until it's only 1/ratio decibels over threshold
+  setInputGainDB(0.0f);//Raising input gain is effectively the same as lowering threshold
+  setAttackTime(5.0f);//experiment with very low values (0.01ms to 10ms)
+  setHoldTime(2.0f);
+  setReleaseTime(20.0f);//generall longer than attack (10 to 100ms)
+  setAnalysisTime(samplesToMS(64));//analysis window size. lower values are more sensitive to input transients.
+  linearGain.setTarget(1.0f);
+}
+float Gate::process(float input){
+  delayLine.inputSample(input);//feed the delay line //no input gain on delayLine sample
+  //apply the input gain
+  input = input * linearInputGain;
+  updateGain(input);
+  //the output is the delayed input scaled by this smoothed linearGain, then multiplied by makeup gain
+  currentSample = delayLine.getDelayed(analysisTime + lookAhead) * 
+                  linearGain.getCurrentValue() * linearMakeUpGain;
+  return currentSample;//return the result
+}
+float Gate::process(float input, float sideChain){//overload for sidechain input
+  delayLine.inputSample(input);//delay line fed input signal still
+  sideChain = sideChain * linearInputGain;//scale sidechain by linearInputGain
+  updateGain(sideChain);//update final gain value with sideChain instead of input
+  //the output is the delayed input scaled by this smoothed linearGain, then multiplied by makeup gain
+  currentSample = delayLine.getDelayed(analysisTime + lookAhead) * 
+                  linearGain.getCurrentValue() * linearMakeUpGain;
+  return currentSample;//return the result
+}
+void Gate::updateGain(float input){
+  //Find the linear intensity of scaled input signal
+  float currentEstimate = signalEstimator.process(input);//store the estimate
+  //debug.printOncePerBuffer(currentEstimate);
+  if(currentEstimate < linearThreshold){//if tested signal is more quite than input
+    
+    //This next portion is particularily expensive as it must call both
+    //amplitudeToDB and dBToAmplitude. Fortunately, this is only called
+    //every time a higher currentEstimate is reached.
+    
+    if(currentEstimate < lowestAttackPhaseTarget){//should only occur if exceeds previous target
+      lowestAttackPhaseTarget = currentEstimate;//record it as the highest
+      attackFlag = true;//make sure compressor is in attack phase if it wasn't already
+      attackPhaseInSamples = 0;//restart the attack phase
+      //how many dB below the threshold was it? How many dB under?
+      if(currentEstimate > 0.0f){
+        float differenceInDB = amplitudeToDB(currentEstimate) - threshold;
+        //how much should the signal be scaled by?
+        reductionTargetDB = (differenceInDB * (ratio)) - differenceInDB;
+        std::cout << currentEstimate << " : " << std::log10(currentEstimate) << std::endl;
+        reductionTargetDB -= rangeDB;//subtract rangeDB value 
+        linearGain.setTarget(dBToAmplitude(reductionTargetDB));//ramp to target amplitue scalar
+      }else{
+        linearGain.setTarget(0.0f);
+      }
+      
+    }
+  }
+  if(attackFlag){//if in attack phase
+    attackPhaseInSamples++;//increment the attackPhase counter
+    //if we have reached the end of the attack+hold phase
+    if(attackPhaseInSamples >= attackTimeInSamples + holdTimeInSamples){
+      lowestAttackPhaseTarget = 1000.0f;//reset the highest for next round
+      attackPhaseInSamples = 0;//reset the attackPhase counter
+      linearGain.setTarget(1.0f);//set target for release
+      attackFlag = false;//change to release state
+    }
+  }
+  linearGain.process();
+}
+void Gate::setThresholdDB(float thresholdDB){
+  lowestAttackPhaseTarget = 1000.0;//previous value may no longer be relavant
+  threshold = thresholdDB;//threshold is stored in decibels
+  linearThreshold = dBToAmplitude(thresholdDB);//efficient to store this, since used often
+}
+void Gate::setRatio(float newRatio){//for every newRatio the input is above the output, reduce output to 1/newRatio over threshold
+  ratio = std::fmax(newRatio, 1.0f);//assign the value, don't allow values below 1.0f
+}
+void Gate::setRangeDB(float newRangeDB){rangeDB = std::max(newRangeDB, 0.0f);}
+void Gate::setInputGainDB(float newInputGainDB){linearInputGain = dBToAmplitude(newInputGainDB);}
+void Gate::setInputGainLinear(float newLinearInputGain){linearInputGain  = newLinearInputGain;}
+void Gate::setMakeUpGainDB(float newMakeUpGainDB){linearMakeUpGain = dBToAmplitude(newMakeUpGainDB);}
+void Gate::setAttackTime(float newAttackTime){
+  linearGain.setTimeDown(newAttackTime);
+  attackPhaseInSamples = 0;//reset the attack phase
+  attackTimeInSamples = msToSamples(newAttackTime);//adjust the attack phase counter
+}
+void Gate::setHoldTime(float newHoldTime){
+  holdTimeInSamples = msToSamples(std::max(newHoldTime, 0.0f));
+}
+void Gate::setReleaseTime(float newReleaseTime){linearGain.setTimeUp(newReleaseTime);}
+void Gate::setLookAheadTime(float newLookAheadTime){
+  lookAhead = std::fmax(newLookAheadTime, 0.0f);
+}
+//lower times (8-32 samples) are more transient-sensitive
+void Gate::setAnalysisTime(float newAnalysisTime){//defaults to 16 samples
+  signalEstimator.setSamplePeriod(samplesToMS(newAnalysisTime));
+}
+float Gate::getSample(){return currentSample;}
+//getLinearScalar() may be used to compress external signals
+float Gate::getLinearScalar(){
+  return linearGain.getCurrentValue();
+}
+float Gate::getThresholdDB(){return threshold;}
+float Gate::getRatio(){return ratio;}
+float Gate::getInputGainDB(){return amplitudeToDB(linearInputGain);}
+float Gate::getMakeUpGainDB(){return dBToAmplitude(linearMakeUpGain);}
+float Gate::getAttackTime(){return linearGain.getTimeUp();}
+float Gate::getReleaseTime(){return linearGain.getTimeDown();}
+float Gate::getLookAheadTime(){return lookAhead;}
