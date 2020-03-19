0ce65705e07e136cc59209ef6a2cda1a7c7f1630
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-03-19 13:29:51 -0700

    more stft

--- include/pedal/STFT.hpp
+++ include/pedal/STFT.hpp
@@ -4,10 +4,44 @@
 Short-Time Fourier Transform
 windowed overlap+add FFT
 */
+#include "../../AudioFFT/AudioFFT.h"//external FFT library
+#include <complex>//std complex data type
+#include <vector>//for dynamic arrays
+#include <cmath>
+
+#include "pedal/HanningWindow.hpp"
+enum WindowTypes{
+  HANNING = 0, 
+  HAMMING, 
+  BLACKMANHARRIS
+};
 
 class STFT{
   public:
-
+  STFT(int initialWindowSize = 512);
+  bool update(float input);//return true if full frame is ready
+  bool isWindowReady();
+  
+  void setWindowType(WindowTypes newWindowType);//not safe to call during analysis
+  void setOverlap(float newOverlap);
+  void setWindowSize(int powOfTwoFFTSize);//must be a power of two(will be adjusted if not)
+  void setBin(int whichBin, std::complex<float> complexInput);
+  int getWindowType();
+  int getOverlap();
+  int getWindowSize();
+  std::complex<float> getBin(int whichBin);
+  
   private:
+  void calculateWindow();
+  int hopSize;
+  int overlap;
+  int windowSize;
+  bool isFrameReady;
+
+  std::vector<float> realBuffer;
+  std::vector<float> imaginaryBuffer;
+  std::vector<float> inputBuffer;
+  std::vector<float> outputBuffer;
+  std::vector<float> window;
 };
 #endif
--- src/spectral/STFT.cpp
+++ src/spectral/STFT.cpp
@@ -1,1 +1,26 @@
 #include "pedal/STFT.hpp"
+
+STFT::STFT(int initialWindowSize){
+  setWindowSize(initialWindowSize);
+  hopSize = windowSize / overlap;
+}
+
+void STFT::setWindowSize(int powerOfTwoSize){
+  //round up to nearest power of 2
+  //from stack overflow:https://stackoverflow.com/questions/466204/rounding-up-to-next-power-of-2
+  windowSize = std::pow(2, std::ceil(std::log(powerOfTwoSize)/
+                                     std::log(2.0f)));
+  window.resize(windowSize);
+  realBuffer.resize(windowSize);
+  imaginaryBuffer.resize(windowSize);
+  inputBuffer.resize(windowSize);
+  outputBuffer.resize(windowSize);
+  calculateWindow();                        
+}
+
+void STFT::calculateWindow(){
+  
+  for(int i = 0; i < windowSize; i++){
+
+  }
+}
