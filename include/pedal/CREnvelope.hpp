c299a068c8788e1b400ea5a2661a0bbfd14554b2
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-03-27 17:19:42 -0700

    more CREnb

--- include/pedal/CREnvelope.hpp
+++ include/pedal/CREnvelope.hpp
@@ -3,7 +3,40 @@
 
 class CREnvelope{
   public:
+  CREnvelope();
+  float generateSample();
+  
+  void setTrigger(bool newTrigger);
+  void setMode(EnvelopeModes newMode);
+  void setAttackTime(float newAttackTime);
+  void setDecayTime(float newDecayTime);
+  void setSustainLevel(float newSustainLevel);
+  void setReleaseTime(float newReleaseTime);
 
+  float getSample();
+  int getState();
+  EnvelopeModes getCurrentMode();
+  float getAttackTime();
+  float getDecayTime();
+  float getSustainLevel();
+  float getReleaseTime();
+  bool getTrigger();
+  bool isBusy();
   private:
+  float currentSample;
+  enum states {OFF = 0, ATTACK, DECAY, SUSTAIN, RELEASE};
+  int currentState;
+  int currentMode;
+  bool trigger;
+  float attackTime;
+  float attackCurveCoefficient;
+  float attackCurveOffset;
+  float decayTime;
+  float decayCurveCoefficient;
+  float decayCurveOffset;
+  float sustainLevel;
+  float releaseTime;
+  float releaseCurveOffset;
+  float releaseCurveCoefficient;
 };
 #endif
