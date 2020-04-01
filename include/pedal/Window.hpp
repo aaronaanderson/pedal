d663a7c6eef97a30ba491d41b5df0341151c32b3
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-03-31 20:50:10 -0700

    prep for Window reconfig

---
+++ include/pedal/Window.hpp
@@ -0,0 +1,14 @@
+#ifndef Window_hpp
+#define Window_hpp
+/*
+Windows are generally used to gradually scale the amplitude of
+an array of samples to zero at both ends. The shape of a window
+has spectral implications. The correct choice of window function
+is dependent upon application. 
+*/
+class Window {
+  public:
+
+  private:
+};
+#endif 
