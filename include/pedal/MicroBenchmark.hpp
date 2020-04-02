33cad6801b817cfd29a3b6557472c7c5055dba34
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-04-02 16:41:36 -0700

    adding MicroBenchmark tool

---
+++ include/pedal/MicroBenchmark.hpp
@@ -0,0 +1,46 @@
+#ifndef MicroBenchmark_hpp
+#define MicroBenchmark_hpp
+
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <algorithm> 
+#include <cmath> 
+
+class MicroBenchmark{
+  public:
+  MicroBenchmark(std::string eventName = "event", long int numberOfIterations = 1000);
+  ~MicroBenchmark();
+  void initialize(std::string eventName, long int numberOfIterations);
+  void startTiming();//start next event timing
+  void stopTiming();//end event timing
+  void pauseTimer();//pause timer mid-event
+  void resumeTimer();//resume timer mid-event
+  void printAllResults();
+  void saveResultsToFile(std::string filePath);
+  void printHighlites();
+  bool getCompleteFlag();
+
+  private:
+  void calculateResults();
+  double calculateAverage();//in microseconds
+  double findMedian();
+  double findFastestInstance();
+  double findSlowestInstance();
+  double average;
+  double slowest;
+  double fastest;
+  double range;
+  std::chrono::time_point<std::chrono::high_resolution_clock> startTime;
+  std::chrono::time_point<std::chrono::high_resolution_clock> endTime;
+  std::chrono::time_point<std::chrono::high_resolution_clock> pauseStartTime;
+  long long   pauseTime;//in microseconds
+  std::vector<long long> timerResults;
+  long  numberOfIterations;
+  long  currentIteration;
+  std::string timerName;
+  bool completeFlag;
+  bool foundStopCall;
+};
+#endif 
