921e234a047129cb998c26e58a1d9f550f715c16
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-04-24 21:02:11 -0700

    first draft edge detect

--- include/pedal/EdgeDetector.hpp
+++ include/pedal/EdgeDetector.hpp
@@ -1,9 +1,29 @@
 #ifndef EdgeDetector_hpp
 #define EdgeDetector_hpp
 
+#include <cmath>
+//returns true if edge detected
+//useful for using a phasor as a timer
 class EdgeDetector{
   public:
+  EdgeDetector();
+
+  bool process(float input);
+  void allowRisingEdge(bool allowRisingEdge);
+  void allowFallingEdge(bool allowFallingEdge);
+  void setRisingThreshold(float newRisingThreshold);
+  void setFallingThreshold(float newFallingThreshold);
+  float getLastFallingEdge();
+  float getLastRisingEdge();
 
   private:
+  bool currentEdge;
+  float previousInput;
+  float risingThreshold;
+  float fallingThreshold;
+  float currentRisingEdge;
+  float currentFallingEdge;
+  bool risingEdgeFlag;
+  bool fallingEdgeFlag;
 };
 #endif
--- src/analysis/EdgeDetector.cpp
+++ src/analysis/EdgeDetector.cpp
@@ -1,1 +1,37 @@
 #include "pedal/EdgeDetector.hpp"
+EdgeDetector::EdgeDetector(){
+  allowRisingEdge(true);
+  allowFallingEdge(true);
+  setRisingThreshold(0.5f);
+  setFallingThreshold(0.5f);
+  currentFallingEdge = 0.0f;
+  currentRisingEdge = 0.0f;
+}
+bool EdgeDetector::process(float input){
+  float delta = std::fabs(input - previousInput);
+  if(risingEdgeFlag){
+    if(delta > risingThreshold){
+      currentEdge = true;
+      currentRisingEdge = delta;
+    }else
+    {
+      currentEdge = false;
+    }
+  }
+  if(fallingEdgeFlag){
+    if(delta > fallingThreshold){
+      currentEdge = true;
+      currentFallingEdge = delta;
+    }else{
+      currentEdge = false;
+    }
+  }
+  previousInput = input;
+  return currentEdge;
+}
+void EdgeDetector::allowRisingEdge(bool allowRisingEdge){risingEdgeFlag = allowRisingEdge;}
+void EdgeDetector::allowFallingEdge(bool allowFallingEdge){fallingEdgeFlag = allowFallingEdge;}
+void EdgeDetector::setRisingThreshold(float newRisingThreshold){risingThreshold = newRisingThreshold;}
+void EdgeDetector::setFallingThreshold(float newFallingThreshold){fallingThreshold = newFallingThreshold;}
+float EdgeDetector::getLastFallingEdge(){return currentFallingEdge;}
+float EdgeDetector::getLastRisingEdge(){return currentRisingEdge;}
